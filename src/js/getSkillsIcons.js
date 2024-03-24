ff01c65f0b0aabb8d69f0f619428cf85852fd9e4
Alejandra <alejandra.cabeza.uk@gmail.com>
2024-03-24 13:16:56 +0000

    add skills icons

---
+++ src/js/getSkillsIcons.js
@@ -0,0 +1,24 @@
+const iconUrls = [
+    'css-icon',
+    'js-icon',
+    'react-icon',
+    'php-icon',
+    'laravel-icon',
+    'mysql-icon',
+    'docker-icon',
+    'aws-icon'
+];
+
+
+const iconContainer = document.getElementById('icon-container');
+
+
+iconUrls.forEach(iconUrl => {
+
+    const iconElement = document.createElement('img');
+    iconElement.src = "./src/assets/skills/" + iconUrl + ".png";
+    iconElement.alt = 'Icon';
+    iconElement.style.width = '54%'
+
+    iconContainer.appendChild(iconElement);
+});
